635e58ba01fe1539672c37b62b650799d1248448
Yan Rodrigues Santos <yyan1207@gmail.com>
2022-03-08 18:05:50 -0300

    Implement intent from MainActivity to GameActivity

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.tictactoe
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.view.ViewCompat
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun openNextActivity(view: View){
+    val intent = Intent(this, GameActivity::class.java)
+    startActivity(intent)
+    view.setOnClickListener{
+        startActivity(intent)
+    }
+
     }
 }
